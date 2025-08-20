c350c202a986169460798ecb1af39dcce3240a7d
Holly Kular <holly@fishee.lab.local>
2025-08-20 10:12:38 -0400

    feedback decoding

--- decode_arg_wrapper.py
+++ decode_arg_wrapper.py
@@ -14,7 +14,7 @@ import itertools
 base_cmd = [
     "python", "decode_multilayer.py",
     "--gpu", "1",
-    "--device", "cuda",
+    "--device", "gpu",
     "--classes", "stim",
     "--stim_noise_train", "0.1",
     "--stim_noise_eval", "0.1",
@@ -23,7 +23,7 @@ base_cmd = [
     "--int_noise_train", "0.1",
     "--int_noise_eval", "0.1",
     "--cue_layer", "3",
-    "--fb32_scalar", "1.0"
+    "--fb21_scalar", "1.0"
 
 ]
 
@@ -33,16 +33,16 @@ cue_on_vals = [0, 75]
 #cue_layer_vals = [3]
 cue_layer = 3
 stim_prob_vals = [16, 70]  # stim_prob_eval will match
-fb21_scalar_vals = [1.0,0.7,0.3,0.15,0]
-#fb32_scalar_vals= [1.0,0.7,0.3,0.15,0]
+#fb21_scalar_vals = [1.0,0.7,0.3,0.15,0]
+fb32_scalar_vals= [1.0,0.7,0.3,0.15,0]
 
 # Loop over all combinations
-for time_or_xgen, cue_on, stim_prob, fb21_scalar in itertools.product(
-    time_or_xgen_vals, cue_on_vals, stim_prob_vals, fb21_scalar_vals
+for time_or_xgen, cue_on, stim_prob, fb32_scalar in itertools.product(
+    time_or_xgen_vals, cue_on_vals, stim_prob_vals, fb32_scalar_vals
 ):
     
     
-    print(f"time_or_xgen={time_or_xgen}, cue_on={cue_on}, cue_layer={cue_layer}, stim_prob={stim_prob}, fb21_s={fb21_scalar}")
+    print(f"time_or_xgen={time_or_xgen}, cue_on={cue_on}, cue_layer={cue_layer}, stim_prob={stim_prob}, fb32_s={fb32_scalar}")
 
     cmd = base_cmd + [
         "--time_or_xgen", str(time_or_xgen),
@@ -50,7 +50,7 @@ for time_or_xgen, cue_on, stim_prob, fb21_scalar in itertools.product(
 #        "--cue_layer", str(cue_layer),
         "--stim_prob_train", str(stim_prob),
         "--stim_prob_eval", str(stim_prob),
-        "--fb21_scalar", str(fb21_scalar),
+        "--fb32_scalar", str(fb32_scalar),
     ]
 
     print(f"Running: {' '.join(cmd)}")
